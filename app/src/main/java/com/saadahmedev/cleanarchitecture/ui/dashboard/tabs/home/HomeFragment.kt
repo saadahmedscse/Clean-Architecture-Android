95f823f59e493980f8bef05a41c31d22c613a6c2
saadahmedscse <saadahmedscse@gmail.com>
2023-10-11 15:09:35 +0600

    get product and product details

--- app/src/main/java/com/saadahmedev/cleanarchitecture/ui/dashboard/tabs/home/HomeFragment.kt
+++ app/src/main/java/com/saadahmedev/cleanarchitecture/ui/dashboard/tabs/home/HomeFragment.kt
@@ -1,9 +1,18 @@
 package com.saadahmedev.cleanarchitecture.ui.dashboard.tabs.home
 
 import android.os.Bundle
+import androidx.fragment.app.viewModels
 import com.saadahmedev.cleanarchitecture.base.BaseFragment
 import com.saadahmedev.cleanarchitecture.databinding.FragmentHomeBinding
+import com.saadahmedev.cleanarchitecture.ui.dashboard.tabs.home.viewmodel.ProductViewModel
+import com.saadahmedev.cleanarchitecture.ui.dashboard.tabs.productDetails.viewmodel.ProductDetailsViewModel
+import com.saadahmedev.cleanarchitecture.util.ProgressDialog
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
@@ -13,7 +22,59 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
     override val isBackButtonVisible: Boolean
         get() = false
 
-    override fun onFragmentCreate(savedInstanceState: Bundle?) {}
+    private val productViewModel: ProductViewModel by viewModels()
+    private val productDetailsViewModel: ProductDetailsViewModel by viewModels()
+    private lateinit var progressDialog: ProgressDialog
 
-    override fun observeData() {}
+    override fun onFragmentCreate(savedInstanceState: Bundle?) {
+        progressDialog = ProgressDialog.getInstance(requireContext())
+        //productViewModel.getProducts(50)
+        productDetailsViewModel.getProduct(2)
+    }
+
+    override fun observeData() {
+        CoroutineScope(Dispatchers.IO).launch {
+            productViewModel.productList.collectLatest {
+                when {
+                    it.isLoading -> {
+                        withContext(Dispatchers.Main) {
+                            progressDialog.show()
+                        }
+                    }
+
+                    it.error.isNotBlank() -> {
+                        progressDialog.dismiss()
+                        it.error.shortSnackBar()
+                    }
+
+                    it.productList.isNotEmpty() -> {
+                        progressDialog.dismiss()
+                        it.productList[0].title.shortSnackBar()
+                    }
+                }
+            }
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            productDetailsViewModel.product.collectLatest {
+                when {
+                    it.isLoading -> {
+                        withContext(Dispatchers.Main) {
+                            progressDialog.show()
+                        }
+                    }
+
+                    it.error.isNotBlank() -> {
+                        progressDialog.dismiss()
+                        it.error.shortSnackBar()
+                    }
+
+                    it.product?.title?.isNotBlank()!! -> {
+                        progressDialog.dismiss()
+                        it.product?.title.shortSnackBar()
+                    }
+                }
+            }
+        }
+    }
 }
